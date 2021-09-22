06f54a12a0a90fbfbffedec76cadf90a9ea45ea7
zabedalam <zabedalam777@gmail.com>
2021-09-22 13:06:50 +0200

    change config

--- functions/util/config.js
+++ functions/util/config.js
@@ -1,10 +1,1 @@
-module.exports = {
-  apiKey: "AIzaSyA2GzLJ3zdNxVcHk78hYFQCi5rQOGL9krw",
-  authDomain: "makeimpact-54613.firebaseapp.com",
-  databaseURL: "https://makeimpact-54613.firebaseio.com",
-  projectId: "makeimpact-54613",
-  storageBucket: "makeimpact-54613.appspot.com",
-  messagingSenderId: "1046682560894",
-  appId: "1:1046682560894:web:ba207a08499e362993ab2f",
-  measurementId: "G-YRSS06DXFM",
-};
+
